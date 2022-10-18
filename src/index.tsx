c06ab43a34ab22da11d7d811071edcefa4d2c524
Li, Roil <Roil.Li@ruckuswireless.com>
2022-10-18 14:41:06 +0800

    Init Project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './assets/styles/index.css';
+import Game from './components/Game';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Game />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
